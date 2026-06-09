d1283370b185c9099316a045650029f4d03322aa
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-06-10 00:56:23 +0600

    login page and register page setup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,122 @@
-import React from "react";
+import '../assets/css/bootstrap.min.css';
+import '../assets/css/common.css';
+import '../assets/css/main.css';
+import '../assets/css/responsive.css';
+import '../assets/js/bootstrap.bundle.min.js';
+
+import shape1 from '../assets/images/shape1.svg';
+import darkShape from '../assets/images/dark_shape.svg';
+import shape2 from '../assets/images/shape2.svg';
+import darkShape1 from '../assets/images/dark_shape1.svg';
+import shape3 from '../assets/images/shape3.svg';
+import darkShape2 from '../assets/images/dark_shape2.svg';
+import loginImg from '../assets/images/login.png';
+import logo from '../assets/images/logo.svg';
+import google from '../assets/images/google.svg';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <section className="_social_login_wrapper _layout_main_wrapper">
+      <div className="_shape_one">
+        <img src={shape1} alt="" className="_shape_img" />
+        <img src={darkShape} alt="" className="_dark_shape" />
+      </div>
+      <div className="_shape_two">
+        <img src={shape2} alt="" className="_shape_img" />
+        <img src={darkShape1} alt="" className="_dark_shape _dark_shape_opacity" />
+      </div>
+      <div className="_shape_three">
+        <img src={shape3} alt="" className="_shape_img" />
+        <img src={darkShape2} alt="" className="_dark_shape _dark_shape_opacity" />
+      </div>
+      <div className="_social_login_wrap">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12">
+              <div className="_social_login_left">
+                <div className="_social_login_left_image">
+                  <img src={loginImg} alt="Image" className="_left_img" />
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+              <div className="_social_login_content">
+                <div className="_social_login_left_logo _mar_b28">
+                  <img src={logo} alt="Image" className="_left_logo" />
+                </div>
+                <p className="_social_login_content_para _mar_b8">Welcome back</p>
+                <h4 className="_social_login_content_title _titl4 _mar_b50">Login to your account</h4>
+                <button type="button" className="_social_login_content_btn _mar_b40">
+                  <img src={google} alt="Image" className="_google_img" />{' '}
+                  <span>Or sign-in with google</span>
+                </button>
+                <div className="_social_login_content_bottom_txt _mar_b40">
+                  <span>Or</span>
+                </div>
+                <form className="_social_login_form">
+                  <div className="row">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_login_form_input _mar_b14">
+                        <label className="_social_login_label _mar_b8">Email</label>
+                        <input type="email" className="form-control _social_login_input" />
+                      </div>
+                    </div>
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_login_form_input _mar_b14">
+                        <label className="_social_login_label _mar_b8">Password</label>
+                        <input type="password" className="form-control _social_login_input" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-12">
+                      <div className="form-check _social_login_form_check">
+                        <input
+                          className="form-check-input _social_login_form_check_input"
+                          type="radio"
+                          name="flexRadioDefault"
+                          id="flexRadioDefault2"
+                          defaultChecked
+                        />
+                        <label
+                          className="form-check-label _social_login_form_check_label"
+                          htmlFor="flexRadioDefault2"
+                        >
+                          Remember me
+                        </label>
+                      </div>
+                    </div>
+                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-12">
+                      <div className="_social_login_form_left">
+                        <p className="_social_login_form_left_para">Forgot password?</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-12 col-md-12 col-xl-12 col-sm-12">
+                      <div className="_social_login_form_btn _mar_t40 _mar_b60">
+                        <button type="button" className="_social_login_form_btn_link _btn1">
+                          Login now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+                <div className="row">
+                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                    <div className="_social_login_bottom_txt">
+                      <p className="_social_login_bottom_txt_para">
+                        Dont have an account? <a href="#0">Create New Account</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,128 @@
-import React from "react";
+import '../assets/css/bootstrap.min.css';
+import '../assets/css/common.css';
+import '../assets/css/main.css';
+import '../assets/css/responsive.css';
+import '../assets/js/bootstrap.bundle.min.js';
+
+import shape1 from '../assets/images/shape1.svg';
+import darkShape from '../assets/images/dark_shape.svg';
+import shape2 from '../assets/images/shape2.svg';
+import darkShape1 from '../assets/images/dark_shape1.svg';
+import shape3 from '../assets/images/shape3.svg';
+import darkShape2 from '../assets/images/dark_shape2.svg';
+import registrationImg from '../assets/images/registration.png';
+import registrationDarkImg from '../assets/images/registration1.png';
+import logo from '../assets/images/logo.svg';
+import google from '../assets/images/google.svg';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className="_social_registration_wrapper _layout_main_wrapper">
+      <div className="_shape_one">
+        <img src={shape1} alt="" className="_shape_img" />
+        <img src={darkShape} alt="" className="_dark_shape" />
+      </div>
+      <div className="_shape_two">
+        <img src={shape2} alt="" className="_shape_img" />
+        <img src={darkShape1} alt="" className="_dark_shape _dark_shape_opacity" />
+      </div>
+      <div className="_shape_three">
+        <img src={shape3} alt="" className="_shape_img" />
+        <img src={darkShape2} alt="" className="_dark_shape _dark_shape_opacity" />
+      </div>
+      <div className="_social_registration_wrap">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12">
+              <div className="_social_registration_right">
+                <div className="_social_registration_right_image">
+                  <img src={registrationImg} alt="Image" />
+                </div>
+                <div className="_social_registration_right_image_dark">
+                  <img src={registrationDarkImg} alt="Image" />
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+              <div className="_social_registration_content">
+                <div className="_social_registration_right_logo _mar_b28">
+                  <img src={logo} alt="Image" className="_right_logo" />
+                </div>
+                <p className="_social_registration_content_para _mar_b8">Get Started Now</p>
+                <h4 className="_social_registration_content_title _titl4 _mar_b50">Registration</h4>
+                <button type="button" className="_social_registration_content_btn _mar_b40">
+                  <img src={google} alt="Image" className="_google_img" />{' '}
+                  <span>Register with google</span>
+                </button>
+                <div className="_social_registration_content_bottom_txt _mar_b40">
+                  <span>Or</span>
+                </div>
+                <form className="_social_registration_form">
+                  <div className="row">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_registration_form_input _mar_b14">
+                        <label className="_social_registration_label _mar_b8">Email</label>
+                        <input type="email" className="form-control _social_registration_input" />
+                      </div>
+                    </div>
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_registration_form_input _mar_b14">
+                        <label className="_social_registration_label _mar_b8">Password</label>
+                        <input type="password" className="form-control _social_registration_input" />
+                      </div>
+                    </div>
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_registration_form_input _mar_b14">
+                        <label className="_social_registration_label _mar_b8">Repeat Password</label>
+                        <input type="password" className="form-control _social_registration_input" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-12 col-xl-12 col-md-12 col-sm-12">
+                      <div className="form-check _social_registration_form_check">
+                        <input
+                          className="form-check-input _social_registration_form_check_input"
+                          type="radio"
+                          name="flexRadioDefault"
+                          id="flexRadioDefault2"
+                          defaultChecked
+                        />
+                        <label
+                          className="form-check-label _social_registration_form_check_label"
+                          htmlFor="flexRadioDefault2"
+                        >
+                          I agree to terms & conditions
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-12 col-md-12 col-xl-12 col-sm-12">
+                      <div className="_social_registration_form_btn _mar_t40 _mar_b60">
+                        <button type="button" className="_social_registration_form_btn_link _btn1">
+                          Login now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+                <div className="row">
+                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                    <div className="_social_registration_bottom_txt">
+                      <p className="_social_registration_bottom_txt_para">
+                        Dont have an account? <a href="#0">Create New Account</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Register;
